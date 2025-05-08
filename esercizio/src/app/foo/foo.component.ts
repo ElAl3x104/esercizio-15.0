f6cb490573289f5c8097133196828efc46aa5a3f
Bonura_Alex <162216566+ElAl3x104@users.noreply.github.com>
2025-05-08 08:49:58 +0000

    completo

--- esercizio/src/app/foo/foo.component.ts
+++ esercizio/src/app/foo/foo.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Foo } from './models/foo.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-foo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './foo.component.html',
   styleUrl: './foo.component.css'
 })
@@ -13,6 +15,11 @@ export class FooComponent{
   data!: Object;
   loading!: boolean;
   o! : Observable<Object>;
+  fooData! : Foo[];
+  oPost! : Observable<Object>;
+  oFoo! : Observable<Foo[]>; 
+
+
 
   http: HttpClient;
   constructor(http: HttpClient){
@@ -35,4 +42,36 @@ export class FooComponent{
     this.data = d; //Notifico l’oggetto ricevuto dal server
     this.loading = false;  // Notifico che ho ricevuto i dati
   }
+
+  makePost(): void {
+    // Definisco i dati da spedire
+    let dataToSend = JSON.stringify({ 
+      body: 'bar',
+      title: 'foo',
+      userId: 1
+    });
+
+    this.loading = true;
+
+    //Faccio la richiesta post
+    this.oPost = this.http.post('https://jsonplaceholder.typicode.com/posts', dataToSend)
+    this.oPost.subscribe(this.getPostResponse);
+  }
+
+   //Ricevo i dati in risposta dalla post (Nota, non sono ancora tipizzati)
+   getPostResponse = (data : Object) => {
+    this.data = data;
+    this.loading = false;
+  }
+  
+
+
+
+  makeTypedRequest() : void
+ {
+   //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+   this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+   this.oFoo.subscribe(data => {this.fooData = data;});
+ }
+
 }
